2bcac023099237aab21d2061be684e6e40e36538
Jean Bahnik <jeanbahnik@gmail.com>
2017-07-30 13:26:31 -0700

    170/282

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,18 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  raise TriangleError.new if a == 0 || b == 0 || c == 0
+  raise TriangleError.new if a < 0 || b < 0 || c < 0
+  raise TriangleError.new if a+b <= c || b+c <= a || a+c <= b
+  if a == b && b == c
+    return :equilateral
+  elsif a == b || b == c || a == c
+    return :isosceles
+  elsif a != b && b != c && a != c
+    return :scalene
+  else
+    raise TriangleError.new
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
